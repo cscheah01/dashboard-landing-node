0b0129af88a8c3bd9f9c84fb2ffaaf0cb5ee9be1
alfred <cscheah01@gmail.com>
2026-05-11 23:05:22 +0800

    Prepare Express backend for Render deployment

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env"), quiet: true });
+
 const express = require("express");
 const cors = require("cors");
 const dashboardRoutes = require("./routes/dashboardRoutes");
@@ -5,11 +8,27 @@ const clientsRoutes = require("./routes/clientsRoutes");
 const leadsRoutes = require("./routes/leadsRoutes");
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 
-app.use(cors());
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        return;
+      }
+
+      callback(null, false);
+    },
+  }),
+);
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({
